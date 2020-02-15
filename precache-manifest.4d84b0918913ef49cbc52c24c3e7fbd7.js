3e2aa1195af767a288401cbe7ec9dacc08c1f99d
Jonathan Wideman <jonathan.m.wideman@gmail.com>
2020-02-15 17:39:54 -0500

    Updates

---
+++ precache-manifest.4d84b0918913ef49cbc52c24c3e7fbd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6fca97959de9d696cc85359a9fcf2c",
+    "url": "/ask-the-oracle/index.html"
+  },
+  {
+    "revision": "9d2ba3d1dcbc86b20a04",
+    "url": "/ask-the-oracle/static/css/main.2367e55f.chunk.css"
+  },
+  {
+    "revision": "6a2d6b58354a33913454",
+    "url": "/ask-the-oracle/static/js/2.84c29406.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ask-the-oracle/static/js/2.84c29406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d2ba3d1dcbc86b20a04",
+    "url": "/ask-the-oracle/static/js/main.4f9bec97.chunk.js"
+  },
+  {
+    "revision": "7c05b82ca0b6b0a2d59e",
+    "url": "/ask-the-oracle/static/js/runtime-main.7796ceec.js"
+  }
+]);
